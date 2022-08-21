b9eec9414e9cef6e7dfc97c517a8ad6c08a7ac2c
Yacine Merzouk <yacine@merzouk.ca>
2022-08-20 21:28:22 -0400

    1.0.7 Updated package dependencies

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-location',
   summary: 'Fork of iron:location for Firestorm',
-  version: '1.0.5',
+  version: '1.0.7',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-location'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@2.3.6');
+  api.versionsFrom('METEOR@2.7.3');
 
   api.use('underscore');
   api.use('tracker');
   api.use('jquery@3.0.0');
 
-  api.use('meteorhubdotnet:firestorm-iron-core@1.0.5');
-  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.5');
+  api.use('meteorhubdotnet:firestorm-iron-core@1.0.7');
+  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.7');
 
-  api.use('meteorhubdotnet:firestorm-iron-url@1.0.5');
+  api.use('meteorhubdotnet:firestorm-iron-url@1.0.7');
 
   api.use('appcache', {weak: true});
 
