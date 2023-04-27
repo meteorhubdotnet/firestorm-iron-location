197c3356d9e0f8b801e70cf2f10846f9e025f03a
Yacine Merzouk <yacine@merzouk.ca>
2023-04-26 20:42:29 -0400

    1.0.15

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'meteorhubdotnet:firestorm-iron-location',
   summary: 'Fork of iron:location for Firestorm',
-  version: '1.0.10',
+  version: '1.0.15',
   git: 'https://github.com/meteorhubdotnet/firestorm-iron-location'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@2.7.3');
+  api.versionsFrom('METEOR@2.11.0');
 
   api.use('underscore');
   api.use('tracker');
   api.use('jquery@3.0.0');
 
-  api.use('meteorhubdotnet:firestorm-iron-core@1.0.10');
-  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.10');
+  api.use('meteorhubdotnet:firestorm-iron-core@1.0.15');
+  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.15');
 
-  api.use('meteorhubdotnet:firestorm-iron-url@1.0.10');
+  api.use('meteorhubdotnet:firestorm-iron-url@1.0.15');
 
   api.use('appcache', {weak: true});
 
@@ -24,12 +24,4 @@ Package.onUse(function (api) {
   api.addFiles('lib/location.js', 'client');
 
   api.export(['urlToHashStyle', 'urlFromHashStyle'], 'client', {testOnly: true});
-});
-
-Package.onTest(function (api) {
-  api.use('meteorhubdotnet:firestorm-iron-location');
-  api.use('tinytest');
-  api.use('test-helpers');
-
-  api.addFiles('test/location_test.js', 'client');
 });
